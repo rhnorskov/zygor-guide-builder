9bfaf7bc476bc6bb7a1a991bb68dffa3a5a91834
Rasmus Nørskov <rasmus@norskov.org>
2025-07-18 20:46:41 +0200

    .

--- example/out/ZygorGuideBuilder.lua
+++
@@ -1,8 +0,0 @@
-local ZygorGuidesViewer = ZygorGuidesViewer
-
-if not ZygorGuidesViewer then
-    print("Zygor Guide Builder: ZygorGuidesViewer not found! Make sure it's installed and enabled.")
-    return
-end
-
-print("Zygor Guide Builder: Core loaded. Guide files will load next...")
